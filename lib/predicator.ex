73db2ce0c32de6ed02c6ed8e1636c7776d056560
joshua.richardson <jrichocean@gmail.com>
2017-05-18 16:32:36 -0600

    UPDATES for 'lit' and 'not' instructions

--- lib/predicator.ex
+++ lib/predicator.ex
@@ -1,7 +1,51 @@
+defmodule Evaluator do
+  defstruct instructions: [], stack: [], ip: 0
+
+  # inst = [["lit", 30], ["lit", 432], ["compare", "GT"]]
+  # inst = [["lit", true]]
+
+  def execute(inst) do
+    machine = Map.put(%Evaluator{}, :instructions, inst)
+    _execute(get_instruction(machine), machine)
+  end
+
+  defp _execute(nil, machine) do
+    hd(machine.stack)
+  end
+
+  defp _execute(["lit"|[val|_]], machine=%Evaluator{}) do
+
+    machine = %Evaluator{ machine |
+      stack: [val|machine.stack],
+      ip: machine.ip + 1
+    }
+
+    _execute(get_instruction(machine), machine)
+  end
+
+  defp _execute(["not"|_], machine=%Evaluator{}) do
+    val = machine.stack |> List.pop_at(0)
+    machine = %Evaluator{ machine |
+      stack: [!val|machine.stack],
+      ip: machine.ip + 1
+    }
+    _execute(get_instruction(machine), machine)
+  end
+
+  defp get_instruction(machine=%Evaluator{}) do
+    if machine.ip < Enum.count(machine.instructions) do
+      Enum.at(machine.instructions, machine.ip)
+    else
+      nil
+    end
+  end
+end
+
+
+
 defmodule Predicator do
   @moduledoc """
   Documentation for Predicator.
   """
-
 
 end
--- test/predicator_test.exs
+++ test/predicator_test.exs
@@ -1,6 +1,53 @@
 defmodule PredicatorTest do
   use ExUnit.Case
+  import Evaluator
 
+  test "execute/1 returns true" do
+    inst = [["lit", true]]
+    assert execute(inst) == true
+  end
 
+  # @tag :skip
+  test "execute/1 returns false" do
+    inst = [["lit", false]]
+    assert execute(inst) == false
+  end
+
+  # @tag :skip
+  test "execute/1 returns not" do
+    inst = [["lit", true], ["not"]]
+    assert execute(inst) == false
+  end
+
+  @tag :skip
+  test "execute/1 returns integer equal integer" do
+    inst = [["lit", 1], ["lit", 1], ["compare", "EQ"]]
+  end
+
+  @tag :skip
+  test "execute/1 returns variable equal integer" do
+    inst = [["load", "age"], ["lit", 21], ["compare", "EQ"]]
+  end
+
+  @tag :skip
+  test "execute/1 returns integer greater than integer" do
+    inst = [["lit", 2], ["lit", 1], ["compare", "GT"]]
+    assert execute(inst) == []
+  end
+
+  @tag :skip
+  test "execute/1 returns true and true" do
+    inst = [["lit", true], ["jfalse", 2], ["lit", true]]
+  end
+
+  @tag :skip
+  test "execute/1 returns true or false" do
+    inst = [["lit", true], ["jtrue", 2], ["lit", false]]
+  end
+
+  @tag :skip
+  test "execute/1 returns false or integer equal integer" do
+    inst = [["lit", false], ["jtrue", 4], ["lit", 1], ["lit", 1], ["compare", "EQ"]]
+  end
 
 end
