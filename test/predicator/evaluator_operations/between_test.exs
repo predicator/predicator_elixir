57b1410b52a42a7a4ee109f9e489a92fd28cfffd
joshua.richardson <jrichocean@gmail.com>
2018-12-05 14:23:36 -0700

    UPDATES/ADDS BETWEEN instruction for dates

--- test/predicator/evaluator_operations/between_test.exs
+++ test/predicator/evaluator_operations/between_test.exs
@@ -2,15 +2,43 @@ defmodule Predicator.EvaluatorOperation.BetweenTest do
   use ExUnit.Case
   import Predicator.Evaluator
 
+  defmodule TstStruct, do: defstruct [created_at: "2012-12-12"]
+
   describe "[\"BETWEEN\"] operation" do
     test "inclusive evaluation of integer BETWEEN integers" do
-      inst = [["lit", 3], ["lit", 1], ["lit", 5], ["compare", "BETWEEN"]]
+      inst  = [["lit", 3], ["lit", 1], ["lit", 5], ["compare", "BETWEEN"]]
       inst2 = [["lit", 1], ["lit", 1], ["lit", 5], ["compare", "BETWEEN"]]
       inst3 = [["lit", 5], ["lit", 1], ["lit", 5], ["compare", "BETWEEN"]]
 
       assert execute(inst) == true
       assert execute(inst2) == true
       assert execute(inst3) == true
+    end
+
+    test "date eval true between dates" do
+      inst = [
+        ["lit", "2012-12-12"],
+        ["to_date"],
+        ["lit", 1],
+        ["to_date"],
+        ["lit", 5000000000],
+        ["to_date"],
+        ["compare", "BETWEEN"]
+      ]
+      assert execute(inst) == true
+    end
+
+    test "date eval with load is true between dates" do
+      inst = [
+        ["load", "created_at"],
+        ["to_date"],
+        ["lit", 259200],
+        ["to_date"],
+        ["lit", 5000000000],
+        ["to_date"],
+        ["compare", "BETWEEN"]
+      ]
+      assert execute(inst, %TstStruct{}) == true
     end
   end
 end
