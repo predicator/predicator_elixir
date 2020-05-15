b3ac9ef63882e3d2a1714281079879f6a85b679f
Joshua Richardson <joshua@nav.com>
2020-05-15 15:18:18 -0600

    bumps version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Predicator.Mixfile do
   def project() do
     [
       app: :predicator,
-      version: "0.8.1",
+      version: "0.9.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
